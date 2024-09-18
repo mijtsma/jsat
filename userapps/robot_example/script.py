2d216acad982a4daa2961f51a3ccc0e415cb6535
SalHargis <156012241+SalHargis@users.noreply.github.com>
2024-09-18 11:21:47 -0400

    Deleted QOS logic
    We made the decision to make the code simpler for creating strategies, one can manually input the edges that are desired to be highlighted. This is opposed to using the logic for recursively tracing through the graph to create a highlighted strategy based on an out of date QOS value

--- userapps/robot_example/script.py
+++ userapps/robot_example/script.py
@@ -183,143 +183,6 @@ def custom_user_encode(user_data):
     return user_data
 
 
-# Create all combinations of QOS values for the edges
-qos_combinations = list(itertools.product(*edge_qos_values.values()))
-
-# Loop over each combination to create copies of the main_net with these QOS values
-for qos_combo in qos_combinations:
-    # Create a deep copy of the main_net to modify
-    temp_net = copy.deepcopy(main_net)
-    
-    # Set the QOS values for the edges in the temp_net
-    for edge, qos_value in zip(edge_qos_values.keys(), qos_combo):
-        if edge[0] == "confirmation":
-            confirmation_nodes = [node for node in temp_net.get_node_ids() if node.startswith("Confirmation-")]
-            for full_node_id in confirmation_nodes:
-                node_id_after_dash = full_node_id.split("-", 1)[1]
-                print(full_node_id,node_id_after_dash)
-                temp_net.get_edge(full_node_id, node_id_after_dash).user_data = UserData(qos_value)
-        else:
-            print(edge[0])
-            temp_net.get_edge(*edge).user_data = UserData(qos_value)
-    
-    # Write the data to a JSON file using the JSONEncoder and custom_user_encode
-    # The file name includes the QOS values for identification
-    file_name = "data/NSFCareerProj/ForWMCreading/PPtoNWS_{}_conf_{}.json".format(*qos_combo)
-    JSONEncoder.encode(file_name, temp_net, e_user_data_func=custom_user_encode)
-
-
-
-# Manually set the QOS for NWP to RM to 0. This is necessary because WMC schedules NWS but for 
-# the network analysis, this edge must be triggered
-main_net.get_edge("NWP","RM").user_data = UserData(0)
-
-
-# Now add attributes that represent when each BaseEnvironmentResource and OrgResource was last updated
-for node_id in main_net.get_node_ids():
-    
-    node = main_net.get_node(node_id)
-    
-    # Check if BaseEnvironmentResource
-    if issubclass (node.__class__, nd.BaseEnvironmentResource) or issubclass (node.__class__, nd.CoordinationGroundingResource):
-        
-        user_data = lambda: None # Lambda function that we can add attributes to, could use to set last_update_time to zero.
-        user_data.last_update_time = 0#-1000000 # Something really small so that it updates the first time we run it.
-        node.user_data = user_data
-
-# Check that previous is actually working
-# for node_id in main_net.get_node_ids():
-    
-#     node = main_net.get_node(node_id)
-    
-#     # Check if BaseEnvironmentResource
-#     if issubclass (node.__class__, nd.BaseEnvironmentResource) or issubclass (node.__class__, nd.CoordinationGroundingResource):
-        
-#         print(node.user_data.last_update_time)
-
-# Check that QOS is loaded properly
-# for edge_id in main_net.get_edge_ids():
-    
-#     edge = main_net.get_edge(edge_id[0],edge_id[1])
-    
-#     print(edge.user_data.QOS)
-
-# Now we can do some funky things with the QOS requirements...
-# First want to find edges that need updating based on when resources were last updated.
-current_time = 32 #Needs something different here
-
-out_of_bound_edges = []
-
-# Identify which edges are out of QOS bound
-for edge_id in main_net.get_edge_ids():
-    
-    edge = main_net.get_edge(edge_id[0],edge_id[1])
-    
-    # Check if the target is a function node
-    target_node = main_net.get_node(edge_id[1])
-    if issubclass (target_node.__class__, nd.ActionNode):
-        
-        # Check when resource was last updated
-        source_node = main_net.get_node(edge_id[0])
-        last_update_time = source_node.user_data.last_update_time
-        QOS = edge.user_data.QOS
-
-        if float(current_time) - float(last_update_time) > float(QOS):
-            print("QOS for",source_node.id,"to",target_node.id,"is out of bounds with QOS",float(QOS),"and last update time",last_update_time)
-
-            out_of_bound_edges.append(edge_id)
-
-# This function is used to identify functional nodes are predependencies for a target function
-# based on QOS bounds and when resources were last updated.
-# This function takes in a node id of a FUNCTION node that is the target
-# It returns a list of function and resource nodes that need to be updated/executed to abide by QOS requirements
-def add_predecessors_to_strategy(node_id, out_of_bound_edges, strategy_list, main_net):
-
-    # Get the node handle
-    node = main_net.get_node(node_id)
-
-    # If this node is not yet in the strategy, add it!
-    if node not in strategy_list:
-        strategy_list.append(node)
-
-    # Now loop over all incoming edges
-    for pred_node_id in main_net.get_graph().predecessors(node_id):
-
-        # And check whether it is within QOS bound
-        if (pred_node_id, node_id) in out_of_bound_edges:
-
-            # If out of QOS bound, need to add to our strategy (in need for an update)
-            strategy_list.append(main_net.get_node(pred_node_id))
-
-            # We need to add each of the functions that is linked to set this predecessor node
-            # and perform the same check to see if we need to add their predecessors
-            for pred_pred_node_id in main_net.get_graph().predecessors(pred_node_id):
-
-                if main_net.get_node(pred_pred_node_id) not in strategy_list:
-                    add_predecessors_to_strategy(pred_pred_node_id, out_of_bound_edges, strategy_list, main_net)
-
-    return strategy_list
-
-# Identify the strategy
-strategy_list = add_predecessors_to_strategy('RM', out_of_bound_edges, [], main_net)
-
-# Identify edges to highlight (that are connecting the nodes in the strategy)
-connecting_edges = []
-for node in strategy_list:
-    for neighbor in main_net.get_graph().neighbors(node.id):
-        if main_net.get_node(neighbor) in strategy_list:
-            edge = (node.id, neighbor) if (node.id, neighbor) in main_net.get_edge_ids() else (neighbor, node.id)
-            if edge not in connecting_edges:
-                connecting_edges.append(edge)
-
-# Identify all incoming edges for every node in the strategy_list
-connecting_edges = []
-for node in strategy_list:
-    print(node.id)
-    for incoming_edge in main_net.get_graph().in_edges(node.id):
-        if incoming_edge not in connecting_edges:
-            connecting_edges.append(incoming_edge)
-
 # Let's output as a TikZ graph
 l.visualize(main_net, "tikzout3.tex")
 
@@ -327,8 +190,8 @@ l.visualize(main_net, "tikzout3.tex")
     from JSON.
 '''
 app = CytoscapeApp(data_dict, RoverDataHandler)
-# app = CytoscapeApp(data_dict, RoverDataHandler, connecting_edges)
 # Type in the edges you wish to highlight in format ("node1", "node2") as seen in examples below
+# Examples of highlighting the graph to show the strategy being employed, uncomment the one you wish to visualize
 # Strategy without updating PP for NWS
 # app = CytoscapeApp(data_dict, RoverDataHandler, [("GL", "RM"),("GL", "NWS"),("TM", "NWS"), ("Confirmation-NWS", "NWS"), ("NWS", "NWP"), ("NWP", "Confirming-NWS"), ("Confirming-NWS", "Confirmation-NWS"), ("NWP", "RM"), ("PP", "NWS"), ("PP", "RM")])
 # Fully highlighted
@@ -337,6 +200,5 @@ app = CytoscapeApp(data_dict, RoverDataHandler)
 #                                                    ("Confirmation-NWS", "NWS"),("NWP", "Confirming-NWS"),("Confirming-NWS", "Confirmation-NWS"),("CIMG", "Confirming-IC"),("Confirming-IC", "IC"),("Confirming-IC","Confirmation-IC"),("Confirmation-IC","IC"),("PP","Confirming-RPP"),("Confirming-RPP","Confirmation-RPP"),("Confirmation-RPP","RPP"),
 #                                                    ("OBL","Confirming-BLM"),("Confirming-BLM","Confirmation-BLM"),("Confirmation-BLM","BLM"),("OST","Confirming-TMP"),("Confirming-TMP","Confirmation-TMP"),
 #                                                    ("Confirmation-TMP","TMP")])
-# app = CytoscapeApp(data_dict,RoverDataHandler)
 webbrowser.open_new("http://127.0.0.1:8050")
 app.run()
